67e88d771d6820a20e447faf3bfa227b4ff0b056
jewelrana <tonujewel@gmail.com>
2023-11-07 15:45:26 +0600

    [Test] get user list

--- test/autentication/domain/usecases/get_user_test.dart
+++ test/autentication/domain/usecases/get_user_test.dart
@@ -1,7 +1,33 @@
+import 'package:clean_architecute_bloc/features/authentication/domain/entities/user_data.dart';
+import 'package:clean_architecute_bloc/features/authentication/domain/repositories/authentication_repository.dart';
+import 'package:clean_architecute_bloc/features/authentication/domain/usecases/get_users.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
 
+import 'authentication_reposityry.mock.dart';
 
+void main() {
+  late GetUsers useCases;
+  late AuthenticationRepository repository;
 
-void main(){
+  setUp(() {
+    repository = MockAuthRepo();
+    useCases = GetUsers(repository);
+  });
 
-  
+  const tResponse = [UserData.empty()];
+
+  test("should call [AuthRepo.getUsers] and return [List<UserData>]", () async {
+    //
+    when(() => repository.getUsers()).thenAnswer(
+      (_) async => const Right(tResponse),
+    );
+
+    final result = await useCases();
+    expect(result, equals(const Right<dynamic, List<UserData>>(tResponse)));
+
+    verify(() => repository.getUsers()).called(1);
+    verifyNoMoreInteractions(repository);
+  });
 }
